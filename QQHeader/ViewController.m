15a868c2f1263d92f19243d55df579c11fee2417
itlijunjie <itlijunjie@gmail.com>
2015-12-29 18:08:24 +0800

    modify 优化头像实现
    modify 优化头像实现

--- QQHeader/ViewController.m
+++ QQHeader/ViewController.m
@@ -10,13 +10,15 @@
 
 #import "CustomLayer.h"
 
-#define ViewOffset 70
+#define ViewDiameter 140
 
-#define viewwh 70
+#define ViewRadius (ViewDiameter/2)
 
 @interface ViewController ()
 
 @end
+
+static inline float radians(double degrees) { return degrees * M_PI / 180; }
 
 @implementation ViewController
 
@@ -24,82 +26,189 @@
 {
     [super viewDidLoad];
     
-    [self.view addSubview:[self getView2]];
+    UIView *view2 = [self getView2];
+    view2.center = CGPointMake(85, 200);
+    [self.view addSubview:view2];
+    
+    UIView *view3 = [self getView3];
+    view3.center = CGPointMake(235, 200);
+    [self.view addSubview:view3];
+    
+    UIView *view4 = [self getView4];
+    view4.center = CGPointMake(85, 350);
+    [self.view addSubview:view4];
+
+    UIView *view5 = [self getView5];
+    view5.center = CGPointMake(235, 350);
+    [self.view addSubview:view5];
+    
 }
 
-- (UIView *)getView2 {
+- (UIView *)getView2
+{
     
-    CGFloat diameter = (140 + 140 - sqrtf(2) * 140);
+    CGFloat diameter = (ViewDiameter + ViewDiameter - sqrtf(2) * ViewDiameter);
+    CGFloat r = diameter / 2;
     
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, viewwh + ViewOffset, viewwh + ViewOffset)];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ViewDiameter, ViewDiameter)];
     view.center = self.view.center;
     view.backgroundColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1.0];
     
     CustomLayer *layer0 = [CustomLayer layer];
     layer0.imageName = @"0.png";
     layer0.isClip = NO;
-    layer0.scale = diameter / 70;
-    layer0.frame = [self getRect:CGPointMake(viewwh - sqrtf(2) * diameter / 4, viewwh - sqrtf(2) * diameter / 4) size:CGSizeMake(diameter, diameter)];
+    layer0.scale = r / ViewRadius;
+    layer0.frame = [self getRect:CGPointMake(ViewRadius - sqrtf(2) * diameter / 4, ViewRadius - sqrtf(2) * diameter / 4) size:CGSizeMake(diameter, diameter)];
     [view.layer addSublayer:layer0];
     [layer0 setNeedsDisplay];
     
     CustomLayer *layer1 = [CustomLayer layer];
     layer1.imageName = @"1.png";
     layer1.degrees = 180 - 45;
-    layer1.scale = diameter / 70;
-    layer1.frame = [self getRect:CGPointMake(viewwh + sqrtf(2) * diameter / 4, viewwh + sqrtf(2) * diameter / 4) size:CGSizeMake(diameter, diameter)];
+    layer1.scale = r / ViewRadius;
+    layer1.frame = [self getRect:CGPointMake(ViewRadius + sqrtf(2) * diameter / 4, ViewRadius + sqrtf(2) * diameter / 4) size:CGSizeMake(diameter, diameter)];
     [view.layer addSublayer:layer1];
     [layer1 setNeedsDisplay];
 
     return view;
 }
 
-- (UIView *)getView3 {
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, viewwh + ViewOffset, viewwh + ViewOffset)];
+- (UIView *)getView3
+{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ViewDiameter, ViewDiameter)];
     view.center = self.view.center;
-    view.backgroundColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1.0];
+    view.backgroundColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1.0];\
     
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(0, 0, ViewDiameter, ViewRadius + sqrtf(3) / 2 * ViewRadius);
+    
+    CGPoint center0 = CGPointMake(ViewRadius, ViewRadius / 2);
+    CustomLayer *layer0 = [CustomLayer layer];
+    layer0.imageName = @"0.png";
+    layer0.degrees = 30;
+    layer0.frame = [self getRect:center0 size:CGSizeMake(ViewRadius, ViewRadius)];
+    [layer addSublayer:layer0];
+    [layer0 setNeedsDisplay];
+    
+    CGPoint center1 = CGPointMake(center0.x - ViewRadius * sin(radians(30)), ViewRadius / 2 + ViewRadius * cos(radians(30)));
+    CustomLayer *layer1 = [CustomLayer layer];
+    layer1.imageName = @"3.png";
+    layer1.degrees = 270;
+    layer1.frame = [self getRect:center1 size:CGSizeMake(ViewRadius, ViewRadius)];
+    [layer addSublayer:layer1];
+    [layer1 setNeedsDisplay];
+    
+    CGPoint center2 = CGPointMake(center1.x + ViewRadius, center1.y);
+    CustomLayer *layer2 = [CustomLayer layer];
+    layer2.imageName = @"2.png";
+    layer2.degrees = 180 - 30;
+    layer2.frame = [self getRect:center2 size:CGSizeMake(ViewRadius, ViewRadius)];
+    [layer addSublayer:layer2];
+    [layer2 setNeedsDisplay];
+    
+    CGRect f = layer.frame;
+    f.origin.y = (view.frame.size.height - f.size.height) / 2;
+    layer.frame = f;
+    [view.layer addSublayer:layer];
     return view;
 }
 
-- (UIView *)getView4 {
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, viewwh + ViewOffset, viewwh + ViewOffset)];
+- (UIView *)getView4
+{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ViewDiameter, ViewDiameter)];
     view.center = self.view.center;
     view.backgroundColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1.0];
     
     CustomLayer *layer0 = [CustomLayer layer];
     layer0.imageName = @"0.png";
-    layer0.frame = [self getRect:CGPointMake(viewwh/2, viewwh/2) size:CGSizeMake(viewwh, viewwh)];
+    layer0.frame = [self getRect:CGPointMake(ViewRadius/2, ViewRadius/2) size:CGSizeMake(ViewRadius, ViewRadius)];
     [view.layer addSublayer:layer0];
     [layer0 setNeedsDisplay];
     
     CustomLayer *layer1 = [CustomLayer layer];
     layer1.imageName = @"1.png";
     layer1.degrees = 90;
-    layer1.frame = [self getRect:CGPointMake(ViewOffset + viewwh/2, viewwh/2) size:CGSizeMake(viewwh, viewwh)];
+    layer1.frame = [self getRect:CGPointMake(ViewRadius + ViewRadius/2, ViewRadius/2) size:CGSizeMake(ViewRadius, ViewRadius)];
     [view.layer addSublayer:layer1];
     [layer1 setNeedsDisplay];
     
     CustomLayer *layer2 = [CustomLayer layer];
     layer2.imageName = @"2.png";
     layer2.degrees = 180;
-    layer2.frame = [self getRect:CGPointMake(viewwh/2 + ViewOffset, viewwh/2 + ViewOffset) size:CGSizeMake(viewwh, viewwh)];
+    layer2.frame = [self getRect:CGPointMake(ViewRadius/2 + ViewRadius, ViewRadius/2 + ViewRadius) size:CGSizeMake(ViewRadius, ViewRadius)];
     [view.layer addSublayer:layer2];
     [layer2 setNeedsDisplay];
     
     CustomLayer *layer3 = [CustomLayer layer];
     layer3.imageName = @"3.png";
     layer3.degrees = 270;
-    layer3.frame = [self getRect:CGPointMake(viewwh/2, viewwh/2 + ViewOffset) size:CGSizeMake(viewwh, viewwh)];
+    layer3.frame = [self getRect:CGPointMake(ViewRadius/2, ViewRadius/2 + ViewRadius) size:CGSizeMake(ViewRadius, ViewRadius)];
     [view.layer addSublayer:layer3];
     [layer3 setNeedsDisplay];
     return view;
 }
 
-- (UIView *)getView5 {
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, viewwh + ViewOffset, viewwh + ViewOffset)];
+- (UIView *)getView5
+{
+    CGFloat r = ViewRadius / (2 * sin(radians(54)) + 1);
+    CGFloat diameter = r * 2;
+    
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(0, 0,ViewDiameter, r / tan(radians(36)) + r / sin(radians(36)) + diameter);
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0,ViewDiameter, ViewDiameter)];
     view.center = self.view.center;
     view.backgroundColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1.0];
+
+    CGPoint center0 = CGPointMake(ViewRadius, r);
+    CustomLayer *layer0 = [CustomLayer layer];
+    layer0.imageName = @"0.png";
+    layer0.degrees = 54;
+    layer0.scale = r / ViewRadius;
+    layer0.frame = [self getRect:center0 size:CGSizeMake(diameter, diameter)];
+    [layer addSublayer:layer0];
+    [layer0 setNeedsDisplay];
+    
+    CGPoint center1 = CGPointMake(center0.x - diameter * sin(radians(54)), center0.y + diameter * cos(radians(54)));
+    CustomLayer *layer1 = [CustomLayer layer];
+    layer1.imageName = @"1.png";
+    layer1.degrees = 270 + 72;
+    layer1.scale = r / ViewRadius;
+    layer1.frame = [self getRect:center1 size:CGSizeMake(diameter, diameter)];
+    [layer addSublayer:layer1];
+    [layer1 setNeedsDisplay];
+    
+    CGPoint center2 = CGPointMake(center1.x + diameter * cos(radians(72)), center1.y + diameter * sin(radians(72)));
+    CustomLayer *layer2 = [CustomLayer layer];
+    layer2.imageName = @"2.png";
+    layer2.degrees = 270;
+    layer2.scale = r / ViewRadius;
+    layer2.frame = [self getRect:center2 size:CGSizeMake(diameter, diameter)];
+    [layer addSublayer:layer2];
+    [layer2 setNeedsDisplay];
+    
+    CGPoint center3 = CGPointMake(center2.x + diameter, center2.y);
+    CustomLayer *layer3 = [CustomLayer layer];
+    layer3.imageName = @"3.png";
+    layer3.degrees = 180 + 18;
+    layer3.scale = r / ViewRadius;
+    layer3.frame = [self getRect:center3 size:CGSizeMake(diameter, diameter)];
+    [layer addSublayer:layer3];
+    [layer3 setNeedsDisplay];
+    
+    CGPoint center4 = CGPointMake(center3.x + diameter * cos(radians(72)), center3.y - diameter * sin(radians(72)));
+    CustomLayer *layer4 = [CustomLayer layer];
+    layer4.imageName = @"2.png";
+    layer4.degrees = 90 + 36;
+    layer4.scale = r / ViewRadius;
+    layer4.frame = [self getRect:center4 size:CGSizeMake(diameter, diameter)];
+    [layer addSublayer:layer4];
+    [layer4 setNeedsDisplay];
+    
+    CGRect f = layer.frame;
+    f.origin.y = (view.frame.size.height - f.size.height) / 2;
+    layer.frame = f;
+    [view.layer addSublayer:layer];
     
     return view;
 }
@@ -108,26 +217,10 @@
     NSLog(@"aa");
 }
 
-- (CAAnimation*)scaleAnimation{
-    CABasicAnimation *scaleAnim = [CABasicAnimation animationWithKeyPath:@"transform"];
-    scaleAnim.duration = 0.1;
-    scaleAnim.repeatCount = 1;
-    scaleAnim.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1, 1, 1.0)];
-    scaleAnim.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.5, 0.5, 1.0)];
-    scaleAnim.removedOnCompletion = NO;
-	scaleAnim.fillMode = kCAFillModeForwards;
-	return scaleAnim;
-}
-
 
 - (CGRect)getRect:(CGPoint)center size:(CGSize)size
 {
     return CGRectMake(center.x - size.width / 2, center.y - size.height / 2, size.width, size.height);
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
 }
 
 @end
